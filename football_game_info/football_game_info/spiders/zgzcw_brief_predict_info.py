1ea881b5d5f4bc2b436bd7930c0df3ce036ca6d6
Michael.Lee <leewind19841209@gmail.com>
2019-05-05 12:20:28 +0800

    [dev]

---
+++ football_game_info/football_game_info/spiders/zgzcw_brief_predict_info.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import datetime
+from ..items import FSpiderLotteryPredictInfo
+
+class ZgzcwBriefPredictInfoSpider(scrapy.Spider):
+    name = 'zgzcw_brief_predict_info'
+    allowed_domains = ['zgzcw.com']
+    start_urls = ['http://zgzcw.com/']
+
+    domain = 'http://live.zgzcw.com/ls/AllData.action'
+
+    def start_requests(self):
+
+        fork_date = datetime.datetime.now() + datetime.timedelta(days = -1)
+        current_date = datetime.datetime.now() + datetime.timedelta(days = 1)
+
+        date_list = []
+        while((current_date - fork_date).days >= 1):
+            date_list.append(fork_date)
+            fork_date = fork_date + datetime.timedelta(days=1)
+
+        for bet_date in date_list:
+            yield scrapy.FormRequest(
+                url=self.domain,
+                callback=self.parse,
+                formdata={'code': '201', 'ajax': 'true',
+                          'date': bet_date.strftime("%Y-%m-%d")}
+            )
+
+    def parse(self, response):
+        trs = response.xpath('.//tr')
+
+        for tr in trs:
+
+            matchid = tr.xpath('@matchid').extract_first()
+
+            tds = tr.xpath('.//td')
+
+            status = tds[4].xpath('.//strong/text()').extract_first()
+
+            if status is None or status.strip() != u'未':
+                continue
+
+            game = tds[1].xpath('.//span/text()').extract_first()
+            turn = tds[2].xpath('./text()').extract_first()
+
+            game_date = tds[3].xpath('@date').extract_first()
+            game_date_arr = game_date.split(' ')
+
+            goals_block = tds[6].xpath('.//span/text()').extract_first()
+            goals = goals_block.split('-')
+
+            gs = goals[0] if goals[0] is not '' else 0
+            gd = goals[1] if goals[0] is not '' else 0
+
+            home_team = tds[5].xpath('.//a/text()').extract_first()
+            visit_team = tds[7].xpath('.//a/text()').extract_first()
+
+            result = tds[9].xpath('(.//strong)[1]/text()').extract_first()
+
+            spans = tds[10].xpath('(.//div)[1]//span')
+
+            win_bet_return = spans[0].xpath('./text()').extract_first()
+            win_bet_return = float(
+                win_bet_return) if win_bet_return is not None else float(0)
+
+            draw_bet_return = spans[1].xpath('./text()').extract_first()
+            draw_bet_return = float(
+                draw_bet_return) if draw_bet_return is not None else float(0)
+
+            lose_bet_return = spans[2].xpath('./text()').extract_first()
+            lose_bet_return = float(
+                lose_bet_return) if lose_bet_return is not None else float(0)
+
+            # item = FSpiderBriefInfo(
+            #     matchid=int(matchid),
+            #     status=status.strip(),
+            #     game=game,
+            #     turn=turn,
+            #     home_team=home_team,
+            #     visit_team=visit_team,
+            #     gs=int(gs),
+            #     gd=int(gd),
+            #     gn=int(gs) + int(gd),
+            #     time=game_date_arr[0],
+            #     result=int(result),
+            #     win_bet_return=win_bet_return,
+            #     draw_bet_return=draw_bet_return,
+            #     lose_bet_return=lose_bet_return
+            # )
+
+            item = FSpiderLotteryPredictInfo(
+                matchid=int(matchid),
+                issue=0,
+                status="未开始",
+                game=game,
+                turn=turn,
+                home_team=home_team,
+                visit_team=visit_team,
+                time=game_date_arr[0],
+                win_bet_return=win_bet_return,
+                draw_bet_return=draw_bet_return,
+                lose_bet_return=lose_bet_return
+            )
+
+            yield item
